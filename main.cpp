2d6fa2ad32105391c1fedc529433183ff2270201
JuliaEidelwein <jeidelwein@inf.ufrgs.br>
2019-04-08 19:47:22 -0300

    Reading input and getting the size of the puzzle

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 #include "puzzle.hpp"
 
 int main(int argc, char* argv[]){
-    std::cout << argc << std::endl;
-    if(argc > 1){
-        std::cout << argv[1] << std::endl;
+    char* algorithmType;
+    int puzzleSize = -1;
+    if(argc > 2){
+        algorithmType = argv[1];
+        for(int i = 2; i < argc; i++){
+            std::cout << argv[i] << std::endl;
+            if(argv[i][1]){
+                puzzleSize = i - 1;
+            }
+        }
+        if(puzzleSize == -1){
+            puzzleSize = argc - 2;
+        }
+    } else {
+        std::cout << "Incorrect number of arguments!" << std::endl;
+        std::cout << "Use: ./main -<algType> <instance>" << std::endl;
+        return 0;
     }
+    std::cout << "Esse é um " << puzzleSize - 1 << "-Puzzle!" << std::endl;
+    std::cout << argc << std::endl;
     return 0;
 }
