0168155ae0cb5a1cfe85f80c0d2d923c1b8f1a09
JuliaEidelwein <jeidelwein@inf.ufrgs.br>
2019-04-08 20:34:05 -0300

    Filling int array with initial state

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cstdlib>
 #include "puzzle.hpp"
 
 int main(int argc, char* argv[]){
     char* algorithmType;
-    int puzzleSize = -1;
+    int puzzleSize = -1, numOfInstances = -1;
     if(argc > 2){
         algorithmType = argv[1];
         for(int i = 2; i < argc; i++){
@@ -15,6 +16,7 @@ int main(int argc, char* argv[]){
         if(puzzleSize == -1){
             puzzleSize = argc - 2;
         }
+        numOfInstances = (argc - 2)/puzzleSize;
     } else {
         std::cout << "Incorrect number of arguments!" << std::endl;
         std::cout << "Use: ./main -<algType> <instance>" << std::endl;
@@ -22,5 +24,15 @@ int main(int argc, char* argv[]){
     }
     std::cout << "Esse é um " << puzzleSize - 1 << "-Puzzle!" << std::endl;
     std::cout << argc << std::endl;
+
+    int *initialState;
+    initialState = (int*) malloc(puzzleSize*sizeof(int));
+    for(int i = 0; i < numOfInstances; i++){
+        initialState = fill_initial_state(initialState, argv, puzzleSize, i);
+        for(int i = 0; i < puzzleSize; i++){
+            std::cout << initialState[i];
+        }
+    }
+
     return 0;
 }
---
+++ puzzle.hpp
@@ -0,0 +1,1 @@
+int * fill_initial_state(int initialState[], char* argv[], int puzzleSize, int instance);
