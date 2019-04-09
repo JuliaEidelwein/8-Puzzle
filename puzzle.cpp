23b5b062b065d0c148f87a1bd5603865bcbf7109
JuliaEidelwein <jeidelwein@inf.ufrgs.br>
2019-04-09 11:59:37 -0300

    Counting zero position

--- puzzle.cpp
+++ puzzle.cpp
@@ -4,15 +4,38 @@
 
 unsigned long long fill_initial_state(unsigned long long initialState, char* argv[], int puzzleSize, int instance){
     int startPosition = (2 + instance*puzzleSize), endPosition = (2 + (1+instance)*puzzleSize);
-    int i, j;
-    for(i = startPosition, j = 0; i < endPosition - 1; i++, j++){
+    int i, entry, shiftAmount = 0, zeroPosition = 0;
+    for(i = startPosition; i < endPosition - 1; i++){
         //initialState[j] = atoi(argv[i]);
-        initialState = initialState | atoi(argv[i]);
+        entry = atoi(argv[i]);
+        initialState = initialState | entry;
+        if(entry == 0){
+            zeroPosition = shiftAmount;
+        }
         printf("ATOI %llx\n",initialState);
         initialState = initialState << 4;
+        shiftAmount++;
         printf("SHIFT %llx\n",initialState);
     }
-    initialState = initialState | atoi(argv[i]);
+    entry = atoi(argv[i]);
+    initialState = initialState | entry;
+    if(entry == 0){
+        zeroPosition = shiftAmount;
+    }
     printf("ATOI %llx\n",initialState);
+    zeroPosition = (puzzleSize - 1) - zeroPosition;
+    printf("Zero esta na posicao %d\n",zeroPosition);
     return initialState;
+}
+
+unsigned long long generate_successors(unsigned long long state, int zeroPosition, int direction){
+    unsigned long long newState;
+    switch(direction){
+        case UP:
+
+        case DOWN:
+        case LEFT:
+        case RIGHT:
+            break;
+    }
 }
--- puzzle.hpp
+++ puzzle.hpp
@@ -1,1 +1,5 @@
+enum moves{UP, DOWN, LEFT, RIGHT};
+
 unsigned long long fill_initial_state(unsigned long long initialState, char* argv[], int puzzleSize, int instance);
+
+unsigned long long generate_successors(unsigned long long state, int zeroPosition, int direction);
